2e3e61f2948027bb40daf378b067d4e63285c1cb
bagasandi270 <bagas.andianto270@gmail.com>
2023-07-15 18:27:28 +0700

    2-addCart.cy

--- cypress/integration/pages/2-addCart.cy.ts
+++ cypress/integration/pages/2-addCart.cy.ts
@@ -8,11 +8,7 @@ const URL = 'https://www.saucedemo.com'
 describe('Test Suite Sauce Demo - Add Product to Cart', () => {
     it('TC_003 - Positive Test - Add products to cart', () => {
         loginPage.login(URL, 'standard_user', 'secret_sauce')        
-        dashboardPage.add_SauceLabsBackpack_toCart()
-        dashboardPage.add_SauceLabsBikeLight_toCart()
-        dashboardPage.add_SauceLabsBoltTShirt_toCart()
-        dashboardPage.assert_addedToCart()
-        dashboardPage.openCart()
-        dashboardPage.assert_openCart()
+  
+   
     })
 })
